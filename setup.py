678ff9249ca9f6523017087153eab5db90d62941
Parth Vasoya <parthvasoya@parths-laptop.home>
2024-04-19 08:01:32 -0400

    projectstructureupdated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="chatbot",
+    version="0.0.1",
+    author="Parth",
+    author_email="parthvasoya1000@gmail.com",
+    packages=find_packages(),
+    install_requires=["langchain"]
+)
